ef6ab51e71a6d3aba28511943f98f6813accad88
DeannaShort <113717324+DeannaShort@users.noreply.github.com>
2024-04-04 20:34:11 -0500

    feat: posts crud (#4)

---
+++ api/Features/Posts/GetAllPostsRequest.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using tweeter.Data;
+using tweeter.Shared;
+
+namespace tweeter.Features.Posts;
+
+public class GetAllPostsRequest : IRequest<Response<List<PostGetDto>>> 
+{
+}
+
+public class GetAllPostsRequestHandler : IRequestHandler<GetAllPostsRequest, Response<List<PostGetDto>>>
+{
+    private readonly DataContext _dataContext;
+    private readonly IMapper _mapper;
+
+    public GetAllPostsRequestHandler(DataContext dataContext,
+        IMapper mapper)
+    {
+        _dataContext = dataContext;
+        _mapper = mapper;
+    }
+
+    public async Task<Response<List<PostGetDto>>> Handle(GetAllPostsRequest request, CancellationToken cancellationToken)
+    {
+        var users = await _dataContext.Set<Post>()
+            .AsNoTracking()
+            .ToListAsync(cancellationToken);
+
+        return _mapper.Map<List<PostGetDto>>(users).AsResponse();
+    }
+}
---
+++ api/Features/Posts/UpdatePostRequest.cs
@@ -0,0 +1,62 @@
+﻿using System.Text.Json.Serialization;
+using AutoMapper;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using tweeter.Data;
+using tweeter.Shared;
+
+namespace tweeter.Features.Posts;
+
+public class UpdatePostRequest : PostDto, IRequest<Response<PostGetDto>>
+{
+    [JsonIgnore] public int Id { get; set; }
+}
+
+public class UpdatePostRequestHandler : IRequestHandler<UpdatePostRequest, Response<PostGetDto>>
+{
+    private readonly IValidator<UpdatePostRequest> _validator;
+    private readonly IMapper _mapper;
+    private readonly DataContext _dataContext;
+
+    public UpdatePostRequestHandler(IValidator<UpdatePostRequest> validator,
+        IMapper mapper, DataContext dataContext)
+    {
+        _validator = validator;
+        _mapper = mapper;
+        _dataContext = dataContext;
+    }
+
+    public async Task<Response<PostGetDto>> Handle(UpdatePostRequest request, CancellationToken cancellationToken)
+    {
+        var validationResult = await _validator.ValidateAsync(request, cancellationToken);
+
+        if (!validationResult.IsValid)
+        {
+            var errors = _mapper.Map<List<Error>>(validationResult.Errors);
+            return new Response<PostGetDto>{Errors = errors};
+        }
+
+        var post = await _dataContext.Set<Post>()
+            .Where(x => !x.IsDeleted)
+            .FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+
+        if (post is null)
+        {
+            return Error.AsResponse<PostGetDto>("Post not found", "id");
+        }
+
+        var requestPost = _mapper.Map<Post>(request);
+        _mapper.Map(requestPost, post);
+        await _dataContext.SaveChangesAsync(cancellationToken);
+        return _mapper.Map<PostGetDto>(post).AsResponse();
+    }
+}
+
+public class UpdatePostRequestValidator : AbstractValidator<UpdatePostRequest>
+{
+    public UpdatePostRequestValidator(IValidator<PostDto> baseValidator)
+    {
+        Include(baseValidator);
+    }
+}
